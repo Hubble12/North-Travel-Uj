8c8cae236de373f11968d25d53b4b8d92232d8a9
Varga Dominik <vargadominik@turr.hu>
2026-02-02 09:37:44 +0100

    NTS-26 javascript kezdete

---
+++ buy.js
@@ -0,0 +1,14 @@
+let szemelyekszama;
+let indulas;
+let hazateres;
+let szobak;
+let email;
+
+function fizetes (){
+    szemelyekszama = document.getElementById("input1").value
+    indulas = document.getElementById("input2").value
+    hazateres = document.getElementById("input3").value
+    szobak = document.getElementById("input4").value
+    email = document.getElementById("input5").value
+    alert(szemelyekszama)
+}
