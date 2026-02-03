4ca88602d2d3f8ef574c26af2f662b97361d716c
Varga Dominik <vargadominik@turr.hu>
2026-02-03 12:24:53 +0100

    bugfix

--- buy.js
+++ buy.js
@@ -5,10 +5,24 @@ let szobak;
 let email;
 
 function fizetes (){
+    //adatbeolvasás
     szemelyekszama = document.getElementById("input1").value
     indulas = document.getElementById("input2").value
     hazateres = document.getElementById("input3").value
     szobak = document.getElementById("input4").value
     email = document.getElementById("input5").value
-    alert(szemelyekszama)
+
+
+    let tartozkodas = 0
+    let honap1 = indulas.substring(2)
+    let honap2 = hazateres.substring(2)
+    //alert(honap1)
+    //alert(honap2)
+
+   // switch(indulas1) {
+   //     case 01:
+
+  //  }
+
+
 }
